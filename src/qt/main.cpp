1d86cb659d3198522844edd36a7708c61ac5fb63
RedDevilus <martensregis@gmail.com>
2020-06-09 23:33:04 +0200

    V2

--- src/qt/main.cpp
+++ src/qt/main.cpp
@@ -1,7 +1,7 @@
 #include <QApplication>
 #include <memory>
 #include "emuwindow.hpp"
-
+#include <QStyleFactory>
 using namespace std;
 
 int main(int argc, char** argv)
@@ -9,14 +9,36 @@ int main(int argc, char** argv)
     QApplication::setOrganizationName("PSI");
     QApplication::setApplicationName("DobieStation");
     QApplication::setOrganizationDomain("https://github.com/PSI-Rockin/DobieStation");
-	QApplication::setStyle(QStyleFactory::create("Modern Dark Theme"));
-	QPalette p;
-	p = qApp->palette();
-	p.setColor(QPalette::Window, QColor(69,69,69));
-	p.setColor(QPalette::Button, QColor(69,69,69));
-	p.setColor(QPalette::Highlight, QColor(50,169,191));
-	p.setColor(QPalette::ButtonText, QColor(169,209,209));
-	qApp->setPalette(p);
+#ifdef Q_OS_WIN //Will make Dobiestation compile to dark if its a windows OS and the user uses dark theme
+    QSettings settings("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",QSettings::NativeFormat);
+    if(settings.value("AppsUseLightTheme")==0){
+        qApp->setStyle(QStyleFactory::create("Fusion"));
+        QPalette darkPalette;
+        QColor darkColor = QColor(45,45,45);
+        QColor disabledColor = QColor(127,127,127);
+        darkPalette.setColor(QPalette::Window, darkColor);
+        darkPalette.setColor(QPalette::WindowText, Qt::white);
+        darkPalette.setColor(QPalette::Base, QColor(18,18,18));
+        darkPalette.setColor(QPalette::AlternateBase, darkColor);
+        darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+        darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+        darkPalette.setColor(QPalette::Text, Qt::white);
+        darkPalette.setColor(QPalette::Disabled, QPalette::Text, disabledColor);
+        darkPalette.setColor(QPalette::Button, darkColor);
+        darkPalette.setColor(QPalette::ButtonText, Qt::white);
+        darkPalette.setColor(QPalette::Disabled, QPalette::ButtonText, disabledColor);
+        darkPalette.setColor(QPalette::BrightText, Qt::red);
+        darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+
+        darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+        darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+        darkPalette.setColor(QPalette::Disabled, QPalette::HighlightedText, disabledColor);
+
+        qApp->setPalette(darkPalette);
+
+        qApp->setStyleSheet("QToolTip { color: #ffffff; background-color: #2a82da; border: 1px solid white; }");
+    }
+#endif
 
     QApplication a(argc, argv);
     auto window = unique_ptr<EmuWindow>(new EmuWindow());
