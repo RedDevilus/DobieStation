5a5a0b0f3243aeb726f37462dbd7e5d4b65fe10f
RedDevilus <martensregis@gmail.com>
2020-06-09 23:15:48 +0200

    Add dark mode
    This will add dark mode to dobiestation

--- src/qt/main.cpp
+++ src/qt/main.cpp
@@ -9,6 +9,14 @@ int main(int argc, char** argv)
     QApplication::setOrganizationName("PSI");
     QApplication::setApplicationName("DobieStation");
     QApplication::setOrganizationDomain("https://github.com/PSI-Rockin/DobieStation");
+	QApplication::setStyle(QStyleFactory::create("Modern Dark Theme"));
+	QPalette p;
+	p = qApp->palette();
+	p.setColor(QPalette::Window, QColor(69,69,69));
+	p.setColor(QPalette::Button, QColor(69,69,69));
+	p.setColor(QPalette::Highlight, QColor(50,169,191));
+	p.setColor(QPalette::ButtonText, QColor(169,209,209));
+	qApp->setPalette(p);
 
     QApplication a(argc, argv);
     auto window = unique_ptr<EmuWindow>(new EmuWindow());
